8a9c22cc47c451c4a8326fda28ae6ffbb731b375
HyunJin Ko <iamkokoko123@gmail.com>
2022-02-02 17:04:26 +0900

    Email, Facebook Login

---
+++ app/src/main/java/com/example/app_study_tinder/LoginActivity.kt
@@ -0,0 +1,144 @@
+package com.example.app_study_tinder
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var callbackManager: CallbackManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+        callbackManager = CallbackManager.Factory.create()
+
+        initLoginButton()
+        initSignUpButton()
+        initEmailAndPasswordEditText()
+        initFacebookLoginButton()
+
+    }
+
+
+    private fun initLoginButton() {
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val email = getInputEmail()
+            val password = getInputPassword()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        finish()
+                    } else {
+                        Toast.makeText(this, "로그인 실패. 이메일 또는 비밀번호 확인 바람", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
+    }
+
+    private fun initSignUpButton() {
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+        signUpButton.setOnClickListener {
+            val email = getInputEmail()
+            val password = getInputPassword()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this,
+                            "회원가입에 성공했습니다. 로그인 버튼을 눌러 로그인해주세요.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(this, "이미 가입한 이메일이거나 회원가입에 실패했습니다.", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+
+        }
+    }
+
+    private fun initEmailAndPasswordEditText() {
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+
+        emailEditText.addTextChangedListener {
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+            signUpButton.isEnabled = enable
+        }
+
+        passwordEditText.addTextChangedListener {
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+            signUpButton.isEnabled = enable
+        }
+
+    }
+
+
+    private fun initFacebookLoginButton() {
+        val facebookLoginButton = findViewById<LoginButton>(R.id.facebookLoginButton)
+
+        facebookLoginButton.setPermissions("email", "public_profile") // 어떤 정보를 가져올지
+        facebookLoginButton.registerCallback(callbackManager, object: FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult) {
+                // 액세스 토큰을 가져와 Firebase 에 알려줌
+                val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener(this@LoginActivity) { task ->
+                        if (task.isSuccessful) {
+                            finish()
+                        } else {
+                            Toast.makeText(this@LoginActivity, "FACEBOOK 로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+
+            override fun onCancel() {}
+
+            override fun onError(error: FacebookException?) {
+                Toast.makeText(this@LoginActivity, "FACEBOOK 로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+
+    private fun getInputEmail(): String {
+        return findViewById<EditText>(R.id.emailEditText).text.toString()
+    }
+
+    private fun getInputPassword(): String {
+        return findViewById<EditText>(R.id.passwordEditText).text.toString()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/app_study_tinder/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.app_study_tinder
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
